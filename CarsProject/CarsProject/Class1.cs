10b9239657880b8b1974b22beb31847605187c03
Jordonjjj <123424266+Jordonjjj@users.noreply.github.com>
2023-01-23 20:27:24 -0500

    First version of main menu
    Adds cars but doesn't delete or use menu right

---
+++ CarsProject/CarsProject/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarsProject
+{
+    internal class Car
+    {
+        public int year;
+        public string? make;
+        public string? model;
+        public int numberofDoors;
+        public double price;
+    }
+}
